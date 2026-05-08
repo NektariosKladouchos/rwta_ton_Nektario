7dddd3868d0eb7ce17ccb7511672de74c266f061
NektariosKladouchos <kladouxos@geyer.gr>
2026-05-08 23:58:47 +0300

    Create 1_Idees.py

---
+++ pages/1_Idees.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Ιδέες & Λύσεις", layout="wide")
+
+st.markdown("# 🏠 Ιδέες Αυτοματισμού")
+st.info("Εδώ θα ανεβάζουμε παρουσιάσεις και λύσεις για DALI, LED και Smart Home.")
