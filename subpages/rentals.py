9aec95d61a3e044f5bc2566b0d21afda5ae81001
NektariosKladouchos <kladouxos@geyer.gr>
2026-05-20 19:41:14 +0300

    Update rentals.py

--- subpages/rentals.py
+++ subpages/rentals.py
@@ -82,7 +82,7 @@ def show():
     st.markdown("""
 <style>
 .carousel-img {
-    max-height: 380px !important;
+    max-height: 330px !important;
     width: 100% !important;
     object-fit: contain !important;
     border-radius: 12px;
