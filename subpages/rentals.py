5b5f4b57dc761bec1c098a85881b4bf9feab223c
NektariosKladouchos <kladouxos@geyer.gr>
2026-05-20 19:37:44 +0300

    Update rentals.py

--- subpages/rentals.py
+++ subpages/rentals.py
@@ -134,7 +134,10 @@ def show():
         # 3. Εμφάνιση της επιλεγμένης εικόνας και της επεξήγησής της
         for img in app_images:
             if img["title"] == selected_app_page:
-                st.image(img["url"], caption=img["title"], use_container_width=True)
+               st.markdown('<div class="carousel-img">', unsafe_allow_html=True)
+st.image(img["url"], use_container_width=True)
+st.markdown('</div>', unsafe_allow_html=True)
+
                 st.info(f"**ℹ️ Επεξήγηση:** {img['desc']}")
 
 
